ca110d8cdfaf9ccb335b884fdaae0630e7f258ea
Roman <pondz.rf@mail.ru>
2020-08-14 18:36:55 +0300

    Initial commit

---
+++ NikeRunClone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NikeRunClone
+//
+//  Created by Roman Tuzhilkin on 8/14/20.
+//  Copyright © 2020 Roman Tuzhilkin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NikeRunClone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NikeRunClone
+//
+//  Created by Roman Tuzhilkin on 8/14/20.
+//  Copyright © 2020 Roman Tuzhilkin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
